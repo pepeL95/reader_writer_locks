fd8482fdf316eb9ba6cbd9166e5d90b917bc7b6a
Jose Lopez <jose.lopezgarrido95@gmail.com>
2022-07-10 09:31:05 -0400

    implemented time wasting function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 # include <stdio.h>
 
+void simulate_read_write(void);
+
 int main(int argc, char * argv) {
     
     
     return 0;
+}
+
+void simulate_read_write(void) {
+    int T = rand() % 10000;
+    for (int i = 0; i < T; i++)
+        for (int j = 0; j < T; j++)
+            continue;
 }
